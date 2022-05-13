77a79a5ab14edb9bcf96f88a1342558c4ae7945c
numan-being <numanahmedh@gmail.com>
2022-05-13 16:43:03 +0530

    added nav bar and text input field

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,40 +1,47 @@
 import React from 'react'
 import { useState } from 'react'
+import logo from '/public/img/handy-downloading.gif'
 import { Switch } from '@headlessui/react'
 
 function Home() {
   const [isEnabled, setIsEnabled] = useState(false);
   return (
 
-    <div className='max-w-7xl m-auto flex flex-col '>
 
-        <div className='mx-12 mt-4 px-8 py-6'>
-            <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-5xl">
-                <span className="block xl:inline">Home</span>{' '}
-                <span className="block text-indigo-600 xl:inline">Page</span>
-            </h1>
+
+
+    <div class="flex items-center justify-center h-screen">
+        <div class="">
+            <img src={logo} alt="loading..." />
         </div>
-
-        <div className='px-8 m-4 mx-20 border-dashed border-2 border-indigo-500 border-opacity-30 rounded-md h-72 '>
-            <h1 className='text-indigo-800 text-center p-8'>
-                Your content goes here!
-            </h1>
-        </div> 
+      
+      <div class="container bg-white rounded-2x1 border shadow-x1 p-8">
+        <div class="flex flex-col items-center space-y-4">
+          <h1 class="font-bold text-4xl text-gray-700 w-4/6 text-center">
+            hello!
+          </h1>
+          <p class="text-sm text-gray-500 text-center w-5/6">
+            Welocome to grammarify.
+          </p>
+          <input
+            type="text"
+            placeholder="Here's where you can fix your mistakes."
+            class="border-2 rounded-lg w-full h-12 px-4"
+          />
+          <button
+            class="bg-green-400 text-white rounded-md hover:bg-purple-500 font-semibold px-4 py-3 w-full"
+          >
+            grammarify!
+          </button>
+        </div>
+      </div>
     
-        <div className='flex flex-col items-center justify-evenly'>
-            <Switch checked={isEnabled} onChange={setIsEnabled} 
-            className={`${ isEnabled ? 'bg-green-500 shadow-green-700' : 'bg-red-500 shadow-red-700'} text-white py-4 rounded-lg shadow-md my-4 w-96`}>
-            {isEnabled && 'Enabled!'}
-            {!isEnabled && 'Not enabled!'}
-            </Switch>
 
-            <div className='mx-6'>
-                <p className='text-gray-700 text-lg'>This switch is from <a className='text-indigo-600' target="_blank" href="https://headlessui.dev/ ">Headless UI. </a>Unstyled, fully accessible UI components.</p>
-            </div>
-
-        </div>
-
+    
     </div>
+
+    
+
   )
 }
 
