8e0cbc7f1822335fc4cd7bfdb59f804152311387
Sanjay M <arjunsanjay0@gmail.com>
2022-07-11 05:45:27 +0530

    added correct sentence view with copy functionality

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,12 @@ module.exports = {
   content: ["./src/**/*.{html,jsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        playfair: ["Playfair Display", "serif"],
+      },
       colors: {
         "main-pink": "#F68ABE",
+        "main-gray": "#101010",
       },
     },
   },
